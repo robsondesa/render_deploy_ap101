154e40671f397643e089980a1e9bc8e1c52abb29
Robson de Sá <89641890+robsondesa@users.noreply.github.com>
2023-12-25 03:08:50 +0000

    att

--- app.py
+++ app.py
@@ -1,17 +1,11 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_auth
 
 estilos = ["https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css", "https://fonts.googleapis.com/icon?family=Material+Icons", dbc.themes.COSMO]
 dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
 # FONT_AWESOME = "https://use.fontawesome.com/releases/v5.10.2/css/all.css"
 
-VALID_USERNAME_PASSWORD_PAIRS = {
-    'melodesa101': 'Mari@2021'
-}
-
 app = dash.Dash(__name__, external_stylesheets=estilos + [dbc_css])
-auth = dash_auth.BasicAuth(app, VALID_USERNAME_PASSWORD_PAIRS)
 
 app.config['suppress_callback_exceptions'] = True
 app.scripts.config.serve_locally = True
